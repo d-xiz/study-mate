14e7ecad1856862a03e9c6cf45a7463c5e551c31
d-xiz <danishmohamed2003@gmail.com>
2026-02-01 16:56:21 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,11 @@ useUnifiedTopology: true,
 .catch((err) => console.log(err));
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://study-mate-ynmd.onrender.com",
+  credentials: true,
+}));
+
 // Allows Express to parse JSON data from incoming requests
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: false }));
