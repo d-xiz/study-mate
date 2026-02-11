b63f680d31614c7de91f6be0639c38ce1d71bf00
d-xiz <danishmohamed2003@gmail.com>
2026-02-11 11:25:28 +0800

    Update LoginPage.jsx
    login name change

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -41,7 +41,7 @@ const LoginPage = ({ setIsLoggedIn }) => {
   return (
     <main className="auth-page group-form">
       <form className="auth-card" onSubmit={handleLogin}>
-        <h2>Student Login</h2>
+        <h2>Student Login 1</h2>
 <div className="form-field">
         <input
           type="text"
